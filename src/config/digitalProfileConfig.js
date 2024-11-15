2d455a92de8b6d5022e4fd48bcfe3dd34b3eea47
Matt Smith <smithryg710@gmail.com>
2024-11-15 15:44:04 -0500

    yogam att

--- src/config/digitalProfileConfig.js
+++ src/config/digitalProfileConfig.js
@@ -9,19 +9,19 @@ const customIcons = {
 const allIcons = { ...customIcons, ...LucideIcons };
 
 export const digitalProfileConfig = {
-  name: "John Doe",
-  initials: "JD",
-  subtitle: "Web Developer",
-  // profileImage: "https://example.com/path/to/your/image.jpg",
-  backgroundColor: "bg-teal-300",
-  textColor: "text-gray-800",
+  name: "Matt Guzman",
+  initials: "MG",
+  subtitle: "Yoga Instructor & Wellness Advocate",
+  profileImage: "https://i.imgur.com/Ho1i0xC.png",
+  backgroundColor: "bg-emerald-700",
+  textColor: "text-neutral-50",
   headerGradient: {
-    from: "from-blue-500",
-    to: "to-purple-500",
+    from: "from-green-500",
+    to: "to-blue-200",
   },
   siteBackground: {
     type: 'color', // or 'gradient'
-    color: 'bg-amber-500', // Tailwind CSS color class
+    color: 'bg-slate-400', // Tailwind CSS color class
     gradient: {
       from: 'from-gray-900',
       to: 'to-gray-800',
@@ -31,7 +31,6 @@ export const digitalProfileConfig = {
   socialLinks: [
     { icon: 'Mail', url: "mailto: ", label: "Email" },
     { icon: 'Linkedin', url: " ", label: "LinkedIn" },
-    { icon: 'Github', url: " ", label: "GitHub" },
     { icon: 'Twitter', url: " ", label: "Twitter" },
     { icon: 'Instagram', url: " ", label: "Instagram" },
     // Add or remove social links as needed
